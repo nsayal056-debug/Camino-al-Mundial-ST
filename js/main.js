081e33431937922111162e9ed80ca9618bf598e0
Nicolas Sayal <nsayal056@gmail.com>
2026-06-08 21:17:12 -0300

    trivia terminda

--- js/main.js
+++ js/main.js
@@ -5,19 +5,19 @@ const preguntas = [
     opciones: [
         {
             texto: "Argentina, Brasil y Uruguay",
-            imagen: "./Img/pregunta1/opcion-a.png"
+            imagen: "./Img/pregunta1/opcion-a.webp"
         },
         {
             texto: "México, Estados Unidos y Canadá",
-            imagen: "./Img/pregunta1/opcion-b.png"
+            imagen: "./Img/pregunta1/opcion-b.webp"
         },
         {
             texto: "España, Francia e Italia",
-            imagen: "./Img/pregunta1/opcion-c.png"
+            imagen: "./Img/pregunta1/opcion-c.webp"
         },
         {
             texto: "Chile, Perú y Bolivia",
-            imagen: "./Img/pregunta1/opcion-d.png"
+            imagen: "./Img/pregunta1/opcion-d.webp"
         }
     ],
     correcta: 1
@@ -28,19 +28,19 @@ const preguntas = [
         opciones: [
         {
             texto: "Hoja de Pino",
-            imagen: "./Img/pregunta2/opcion-a.png"
+            imagen: "./Img/pregunta2/opcion-a.webp"
         },
         {
             texto: "Hoja de Roble",
-            imagen: "./Img/pregunta2/opcion-b.png"
+            imagen: "./Img/pregunta2/opcion-b.webp"
         },
         {
             texto: "Hoja de Olivo",
-            imagen: "./Img/pregunta2/opcion-c.png"
+            imagen: "./Img/pregunta2/opcion-c.webp"
         },
         {
-            texto: "Hola de Arce",
-            imagen: "./Img/pregunta2/opcion-d.png"
+            texto: "Hoja de Arce",
+            imagen: "./Img/pregunta2/opcion-d.webp"
         },
         ],
         correcta: 3
@@ -51,19 +51,19 @@ const preguntas = [
     opciones: [
         {
             texto: "Bandera de Canadá",
-            imagen: "./Img/pregunta3/opcion-a.png"
+            imagen: "./Img/pregunta3/opcion-a.webp"
         },
         {
             texto: "Bandera de Estados Unidos",
-            imagen: "./Img/pregunta3/opcion-b.png"
+            imagen: "./Img/pregunta3/opcion-b.webp"
         },
         {
             texto: "Bandera de México",
-            imagen: "./Img/pregunta3/opcion-c.png"
+            imagen: "./Img/pregunta3/opcion-c.webp"
         },
         {
             texto: "Bandera de Japón",
-            imagen: "./Img/pregunta3/opcion-d.png"
+            imagen: "./Img/pregunta3/opcion-d.webp"
         }
         ],
         correcta: 0
@@ -74,19 +74,19 @@ const preguntas = [
     opciones: [
         {
             texto: "1 mascota",
-            imagen: "./Img/pregunta4/opcion-a.png"
+            imagen: "./Img/pregunta4/opcion-a.webp"
         },
         {
             texto: "2 mascotas",
-            imagen: "./Img/pregunta4/opcion-b.png"
+            imagen: "./Img/pregunta4/opcion-b.webp"
         },
         {
             texto: "3 mascotas",
-            imagen: "./Img/pregunta4/opcion-c.png"
+            imagen: "./Img/pregunta4/opcion-c.webp"
         },
         {
             texto: "4 mascotas",
-            imagen: "./Img/pregunta4/opcion-d.png"
+            imagen: "./Img/pregunta4/opcion-d.webp"
         }
     ],
     correcta: 2
@@ -97,19 +97,19 @@ const preguntas = [
     opciones: [
         {
             texto: "León, Tigre y Oso",
-            imagen: "./Img/pregunta5/opcion-a.png"
+            imagen: "./Img/pregunta5/opcion-a.webp"
         },
         {
             texto: "Alce, Jaguar y Águila",
-            imagen: "./Img/pregunta5/opcion-b.png"
+            imagen: "./Img/pregunta5/opcion-b.webp"
         },
         {
             texto: "Panda, Koala y Canguro",
-            imagen: "./Img/pregunta5/opcion-c.png"
+            imagen: "./Img/pregunta5/opcion-c.webp"
         },
         {
             texto: "Lobo, Zorro y Conejo",
-            imagen: "./Img/pregunta5/opcion-d.png"
+            imagen: "./Img/pregunta5/opcion-d.webp"
         }
     ],
     correcta: 1
@@ -119,19 +119,19 @@ const preguntas = [
     opciones: [
         {
             texto: "Águila",
-            imagen: "./Img/pregunta6/opcion-a.png"
+            imagen: "./Img/pregunta6/opcion-a.webp"
         },
         {
             texto: "Alce",
-            imagen: "./Img/pregunta6/opcion-b.png"
+            imagen: "./Img/pregunta6/opcion-b.webp"
         },
         {
             texto: "Jaguar",
-            imagen: "./Img/pregunta6/opcion-c.png"
+            imagen: "./Img/pregunta6/opcion-c.webp"
         },
         {
             texto: "Lobo",
-            imagen: "./Img/pregunta6/opcion-d.png"
+            imagen: "./Img/pregunta6/opcion-d.webp"
         }
     ],
     correcta: 0
@@ -142,19 +142,19 @@ const preguntas = [
     opciones: [
         {
             texto: "Jaguar",
-            imagen: "./Img/pregunta7/opcion-a.png"
+            imagen: "./Img/pregunta7/opcion-a.webp"
         },
         {
             texto: "Águila",
-            imagen: "./Img/pregunta7/opcion-b.png"
+            imagen: "./Img/pregunta7/opcion-b.webp"
         },
         {
             texto: "Alce",
-            imagen: "./Img/pregunta7/opcion-c.png"
+            imagen: "./Img/pregunta7/opcion-c.webp"
         },
         {
             texto: "Mapache",
-            imagen: "./Img/pregunta7/opcion-d.png"
+            imagen: "./Img/pregunta7/opcion-d.webp"
         }
     ],
     correcta: 2
@@ -165,19 +165,19 @@ const preguntas = [
     opciones: [
         {
             texto: "Águila",
-            imagen: "./Img/pregunta8/opcion-a.png"
+            imagen: "./Img/pregunta8/opcion-a.webp"
         },
         {
             texto: "Alce",
-            imagen: "./Img/pregunta8/opcion-b.png"
+            imagen: "./Img/pregunta8/opcion-b.webp"
         },
         {
             texto: "Mapache",
-            imagen: "./Img/pregunta8/opcion-c.png"
+            imagen: "./Img/pregunta8/opcion-c.webp"
         },
         {
             texto: "Jaguar",
-            imagen: "./Img/pregunta8/opcion-d.png"
+            imagen: "./Img/pregunta8/opcion-d.webp"
         }
     ],
     correcta: 3
@@ -188,19 +188,19 @@ const preguntas = [
     opciones: [
         {
             texto: "Argentina",
-            imagen: "./Img/pregunta9/opcion-a.png"
+            imagen: "./Img/pregunta9/opcion-a.webp"
         },
         {
             texto: "México",
-            imagen: "./Img/pregunta9/opcion-b.png"
+            imagen: "./Img/pregunta9/opcion-b.webp"
         },
         {
             texto: "España",
-            imagen: "./Img/pregunta9/opcion-c.png"
+            imagen: "./Img/pregunta9/opcion-c.webp"
         },
         {
             texto: "Brasil",
-            imagen: "./Img/pregunta9/opcion-d.png"
+            imagen: "./Img/pregunta9/opcion-d.webp"
         }
     ],
     correcta: 1
@@ -211,19 +211,19 @@ const preguntas = [
     opciones: [
         {
             texto: "Solo fútbol",
-            imagen: "./Img/pregunta10/opcion-a.png"
+            imagen: "./Img/pregunta10/opcion-a.webp"
         },
         {
             texto: "Solo competencia",
-            imagen: "./Img/pregunta10/opcion-b.png"
+            imagen: "./Img/pregunta10/opcion-b.webp"
         },
         {
             texto: "Solo entretenimiento",
-            imagen: "./Img/pregunta10/opcion-c.png"
+            imagen: "./Img/pregunta10/opcion-c.webp"
         },
         {
             texto: "La unión entre culturas y países",
-            imagen: "./Img/pregunta10/opcion-d.png"
+            imagen: "./Img/pregunta10/opcion-d.webp"
         }
     ],
     correcta: 3
@@ -257,12 +257,13 @@ function cargarPregunta() {
 
     boton.classList.add("opcion");
 
-    if (typeof opcion === "object") {
-        boton.innerHTML = `
-            <img src="${opcion.imagen}" alt="${opcion.texto}">
-            <span>${opcion.texto}</span>
-        `;
-    } else {
+   if (typeof opcion === "object") {
+    boton.innerHTML = `
+        <img src="${opcion.imagen}" alt="${opcion.texto}" loading="lazy">
+        <span>${opcion.texto}</span>
+    `;
+    }
+     else {
         boton.textContent = opcion;
     }
 
